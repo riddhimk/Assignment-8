d76cacb6508fc5a97fe19bec41afc7677bdcb1cf
Riddhim Kawdia <riddhim10000@gmail.com>
2025-04-18 21:29:34 +0530

    Updated Student.java

--- Student.java
+++ Student.java
@@ -6,3 +6,13 @@ public class Student
     private String batch;
     private double cgpa;
 
+    // constructor of Student class
+    public Student(String name, long prn, String dept, String batch, double cgpa)
+    {
+        this.name = name;
+        this.prn = prn;
+        this.dept = dept;
+        this.batch = batch;
+        this.cgpa = cgpa;
+    }
+
