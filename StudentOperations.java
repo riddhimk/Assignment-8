c0b2f91daf04e024b6edf64e91ac9509f5a3c2d4
Riddhim Kawdia <riddhim10000@gmail.com>
2025-04-18 21:31:12 +0530

    Added method to add student

--- StudentOperations.java
+++ StudentOperations.java
@@ -5,3 +5,26 @@ public class StudentOperations
     // new array-list to store students
     private ArrayList<Student> students = new ArrayList<>();
 
+    // method to add a new student
+    public void addStudent(Student student)
+            throws DuplicatePRNException, InvalidCGPAException
+    {
+        // iterate through each student
+        for (Student s : students)
+        {
+            // throw exception if PRNs are same
+            if (s.getPRN() == student.getPRN())
+            {
+                throw new DuplicatePRNException("Student with PRN " + student.getPRN() + " already exists.");
+            }
+        }
+
+        // throw exception if CGPA is invalid (out of bounds)
+        if (student.getCGPA() < 0.0 || student.getCGPA() > 10.0)
+        {
+            throw new InvalidCGPAException("CGPA must be between 0 and 10.");
+        }
+        students.add(student);
+        System.out.println("Student added successfully!");
+    }
+
